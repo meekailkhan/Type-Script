6004204f5321282e471c5bb05b31f7fbae1d2fcf
pc <meekailkhan123aar@gmail.com>
2024-11-12 15:24:34 +0530

    typescript

---
+++ simple-type.ts
@@ -0,0 +1,12 @@
+// exeplicit type assingnment
+let firstName:string = "meekail";
+// firstName = 32 // this will be show error 'type number no assignable to type string
+firstName = "abcd";
+console.log(firstName); // abcd
+
+
+let lastName = "mujaahid";
+// lastName = 34 // this will be show error 'type number no assignable to type string
+lastName = "efgh";
+console.log(lastName); // efgh
+
